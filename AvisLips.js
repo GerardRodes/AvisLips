82a035a472f98260a4b024c5270e0f43b60ab316
Gerard Rodes <grv94sk@gmail.com>
2016-05-01 17:26:40 +0200

    Optimized loading of audios through AJAX
    Added time visualization MM:SS

--- AvisLips.js
+++ AvisLips.js
@@ -34,7 +34,9 @@
 				timeline = alItem.find('.timeline'),
 				currentTime = alItem.find('.currentTime'),
 				muteButton = alItem.find('.mute'),
-				loopButton = alItem.find('.loop');
+				loopButton = alItem.find('.loop'),
+				volume = alItem.find('.volume'),
+				duration = alItem.find('.duration');
 
 			$.each(elements,
 				(i,el) => {
@@ -42,12 +44,8 @@
 
 					if(element.length > 0){
 
-						if(element.hasClass('duration')){
-							element.html(audio.duration)
-						}
-
 						if(element.hasClass('currentTime')){
-							element.html(audio.currentTime)
+							element.html('0:00')
 						}
 
 						if(element.hasClass('currentSrc')){
@@ -66,6 +64,11 @@
 			)
 
 			var timelineCurrent = timeline.find('.current');
+			var controllerVolume = volume.find('.current');
+
+			audio.onloadeddata = function(){
+				duration.html(getTime(audio.duration))
+			}
 
 			audio.onpause = function(){
 				playButton.removeClass('pause');
@@ -81,7 +84,9 @@
 
 			audio.ontimeupdate = function(){
 				if(currentTime.length > 0){
-					currentTime.html(audio.currentTime)
+					var ms = audio.currentTime;
+
+					currentTime.html(getTime(ms))
 				}
 
 				if (timeline.length > 0) {
@@ -139,11 +144,24 @@
 				var alItem = $(this).closest('.al-item'),
 					audio = alItem.find('audio')[0];
 
-				if($(this).parent().hasClass('timeline')){
-					audio.currentTime = audio.duration * value;
-				}else if($(this).parent().hasClass('volume')){
-					audio.volume = value;
+				audio.currentTime = audio.duration * value;
+			})
+
+			controllerVolume.on('mousedown',function(e){
+				var value = e.offsetX / $(this).width();
+				if(value > 0.99){
+					value = 1;
+				}else if(value < 0.01){
+					value = 0;
 				}
+
+				this.value = value;
+
+				var alItem = $(this).closest('.al-item'),
+					audio = alItem.find('audio')[0];
+
+					audio.volume = value;
+				
 			})
 
 			alItem.addClass('loaded');
@@ -154,3 +172,21 @@
 	}
 
 })(jQuery);
+
+
+function getTime(ms) {
+
+	var secs = ms.toFixed(0),
+		min = 0;
+
+	while(secs >= 60){
+		secs -= 60;
+		min++;
+	}
+
+	if(secs < 10){
+		secs = ('0' + secs).slice(-2)
+	}
+
+	return min+':'+secs;
+}
